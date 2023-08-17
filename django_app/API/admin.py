4b2bcfb48ec5deacf694876b3148ae2e89ef578a
kmj36 <kmj36953695@gmail.com>
2023-08-17 23:47:58 +0900

    [ADD] django admin User, Post model 표시 변경

--- django_app/API/admin.py
+++ django_app/API/admin.py
@@ -1,10 +1,68 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import *
+from django.utils import timezone
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('userid', 'nickname', 'email', 'is_active', 'is_admin', 'is_superuser', 'created_at', 'updated_at', 'last_login')
+    list_filter = ('is_active', 'is_admin', 'is_superuser', 'created_at', 'updated_at', 'last_login')
+    search_fields = ('userid', 'nickname', 'email')
+    readonly_fields = ('created_at', 'updated_at', 'last_login')
+    ordering = ('userid',)
+    
+    fieldsets = (
+        (None, {'fields': ('userid', 'password')}),
+        ('Personal Info', {'fields': ('nickname', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_admin', 'is_superuser')}),
+        ('Important Dates', {'fields': ('created_at', 'updated_at', 'last_login')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('userid', 'nickname', 'email', 'password1', 'password2', 'is_admin'),
+        }),
+    )
+    
+    ordering = ('userid',)
+    filter_horizontal = ()
+    
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('postid', 'userid', 'categoryid', 'title', 'is_secret', 'is_published', 'published_at', 'created_at', 'updated_at')
+    list_filter = ('is_secret', 'published_at', 'created_at', 'updated_at')
+    search_fields = ('userid', 'categoryid', 'title', 'content')
+    readonly_fields = ('userid', 'published_at', 'created_at', 'updated_at')
+    
+    fieldsets = (
+        (None, {'fields': ('userid', 'title', 'content')}),
+        ('Post Feature', {'fields': ('categoryid', 'tagid')}),
+        ('Post Info', {'fields': ('published_at', 'created_at', 'updated_at')}),
+        ('Post Public', {'fields': ('is_secret', 'is_published')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('userid', 'title', 'content', 'categoryid', 'tagid', 'is_secret', 'is_published'),
+        }),
+    )
+    
+    ordering = ('postid',)
+    filter_horizontal = ()
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.userid:
+            obj.userid = request.user
+            
+        if not obj.published_at:
+            obj.published_at = timezone.now()
+            
+        obj.save()
+        
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Tag)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
--- django_app/API/models.py
+++ django_app/API/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from dotenv import load_dotenv
+import os
 # Create your models here.
+
+load_dotenv(verbose=True)
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
@@ -51,6 +54,8 @@ class UserManager(BaseUserManager):
             password=password,
         )
         superuser.is_admin = True
+        if userid == os.environ.get('DJANGO_ADMIN_ID'):
+            superuser.is_superuser = True
         superuser.is_active = True
         superuser.save(using=self._db)
         return superuser
@@ -59,12 +64,11 @@ class User(AbstractBaseUser, PermissionsMixin): # User 테이블 정의, 1:N 관
     userid = models.CharField(verbose_name='User ID', max_length=32, primary_key=True) # userid를 기본키로 설정
     password = models.CharField(verbose_name='User Password', max_length=128)
     nickname = models.CharField(verbose_name='User Nickname', max_length=64, unique=True)
-    email = models.EmailField(verbose_name='User Email', max_length=256, unique=True)
+    email = models.EmailField(verbose_name='User Email', max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True) # 생성 시간
     updated_at = models.DateTimeField(auto_now=True) # 수정 시간
     last_login = models.DateTimeField(null=True) # 마지막 로그인 시간
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
     objects = UserManager()
@@ -90,7 +94,7 @@ class User(AbstractBaseUser, PermissionsMixin): # User 테이블 정의, 1:N 관
 
 class Tag(models.Model): # Tag 테이블 정의, N:M 관계
     tagid = models.AutoField(verbose_name='Tag ID', primary_key=True)
-    userid = models.ForeignKey(User, verbose_name='Tag Creator', on_delete=models.PROTECT, editable=False, null=True)
+    userid = models.ForeignKey(User, verbose_name='Tag Creator', on_delete=models.SET_NULL, null=True, blank=True)
     tagname = models.CharField(verbose_name='Tag Name', max_length=32, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -99,7 +103,7 @@ class Tag(models.Model): # Tag 테이블 정의, N:M 관계
 
 class Category(models.Model): # Category 테이블 정의, 1:N 관계
     categoryid = models.AutoField(verbose_name='Category ID', primary_key=True)
-    userid = models.ForeignKey(User, verbose_name='Category Creator', on_delete=models.PROTECT, editable=False, null=True)
+    userid = models.ForeignKey(User, verbose_name='Category Creator', on_delete=models.SET_NULL, null=True, blank=True)
     categoryname = models.CharField(verbose_name='Category ID', max_length=64, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -108,13 +112,14 @@ class Category(models.Model): # Category 테이블 정의, 1:N 관계
 
 class Post(models.Model): # Post 테이블 정의, 1:N 관계
     postid = models.AutoField(verbose_name='Post ID', primary_key=True)
-    userid = models.ForeignKey(User, verbose_name='Post Creator', on_delete=models.PROTECT, editable=False, null=True)
-    categoryid = models.ForeignKey(Category, verbose_name='Post Category', on_delete=models.CASCADE, null=True, blank=True)
+    userid = models.ForeignKey(User, verbose_name='Post Creator', on_delete=models.SET_NULL, null=True, blank=True)
+    categoryid = models.ForeignKey(Category, verbose_name='Post Category', on_delete=models.SET_NULL, null=True, blank=True)
     tagid = models.ManyToManyField(Tag, verbose_name='Post Tags', blank=True)
     title = models.CharField(verbose_name='Post Title', max_length=128)
     content = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
     is_published = models.BooleanField(default=True)
     is_secret = models.BooleanField(default=False)
     def __str__(self):
@@ -123,7 +128,7 @@ class Post(models.Model): # Post 테이블 정의, 1:N 관계
 class Comment(models.Model): # Comment 테이블 정의, 1:N 관계
     commentid = models.AutoField(verbose_name='Comment ID', primary_key=True)
     postid = models.ForeignKey(Post, verbose_name='Comment Post ID', on_delete=models.CASCADE)
-    userid = models.ForeignKey(User, verbose_name='Comment Creator', on_delete=models.PROTECT, editable=False, null=True)
+    userid = models.ForeignKey(User, verbose_name='Comment Creator', on_delete=models.SET_NULL, null=True, blank=True)
     content = models.CharField(verbose_name='Comment Content', max_length=1024, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
