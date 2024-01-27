673331a13859cc1fdd9d0be4cdb9d2b439c934a6
kmj36 <kmj36953695@gmail.com>
2024-01-27 21:07:55 +0900

    [MODIFY] 어드민 로그인폼 비밀번호 해시화 반영

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django_app.views import Home
+from django_app.views import Home, CustomLoginForm, CustomLoginView
 
 from django.urls import path, include
 
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', Home.as_view()), # Home
     #path('api/', ...), # 추후 API html 개발
     path('api/v1/', include('API.urls')), # API v1
+    path('admin/login/', CustomLoginView.as_view(), name='login'), # Admin Login
     path('admin/', admin.site.urls), # Admin
     path('api-auth/', include('rest_framework.urls')), # DRF Auth
 ]
--- django_app/django_app/views.py
+++ django_app/django_app/views.py
@@ -1,6 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
+from django.contrib.auth import views
+from django import forms
+from hashlib import sha256
+
 
 class Home(APIView): # Home
     permission_classes = [AllowAny]
@@ -8,3 +13,27 @@ class Home(APIView): # Home
         return Response({
                 'v1' : '/api/v1',
             })
+        
+
+class CustomLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'ID'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+        }
+    ))
+    
+class CustomLoginView(views.LoginView):
+    authentication_form = CustomLoginForm
+    template_name='admin/login.html'
+    
+    def post(self, request, *args, **kwargs):
+        request.POST._mutable = True
+        request.POST['password'] = sha256(request.POST['password'].encode()).hexdigest()
+        request.POST._mutable = False
+        return super().post(request, *args, **kwargs)
